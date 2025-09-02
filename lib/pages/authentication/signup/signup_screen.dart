a0ca827fe8d25358f90739fb74543a92f1c8ab54
macsoftechnologies <macsoftechnologies@gmail.com>
2025-09-02 12:40:10 +0530

    updated location changes

--- lib/pages/authentication/signup/signup_screen.dart
+++ lib/pages/authentication/signup/signup_screen.dart
@@ -13,6 +13,9 @@ import '../../../utils/config.dart';
 import 'package:gobuddy/services/end_points.dart';
 import 'package:gobuddy/services/repository.dart';
 
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
 
@@ -26,10 +29,82 @@ class _SignupScreenState extends State<SignupScreen> {
    @override
   void initState() {
     super.initState();
-    callCatDetailsAPI();
+
+   callLocation();
+  
   }
 
+  callLocation() async {
+    UtilClass.showProgress(context: context);
+ var location = await _determinePosition();
 
+ print(location);
+
+ var address =  await _getAddressFromLatLng(location);
+
+ // ignore: avoid_print
+ print(address[0].postalCode ?? "530017");
+ UtilClass.hideProgress();
+  callCatDetailsAPI();
+  }
+
+  Future<List<Placemark>> _getAddressFromLatLng(Position position) async {
+  try {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+    return placemarks;
+  } catch (e) {
+    return Future.error('Error getting address: $e');
+  }
+}
+
+Future<dynamic> _determinePosition() async {
+
+  try{
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the 
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale 
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+  
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately. 
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  } 
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+  }
+  catch(err){
+    print(err);
+  return err;
+    
+
+  }
+}
 callCatDetailsAPI() async {
 
 
